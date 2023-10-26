ed997c80bd25dbdacf45bc30084b719a304ec574
di0rio <126248745+di0rio@users.noreply.github.com>
2023-10-26 13:42:49 +0000

    ajustes alert comprar no Home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -3,17 +3,27 @@ import style from "./Home.module.css";
 import React, { useState, useEffect } from "react";
 
 import mine from "../../assets/mine.jpg";
+import Swal from "sweetalert2";
 
 const Home = () => {
-  const [value, setValue] = useState("");
+  // const [value, setValue] = useState("");
 
   const handleClick = async () => {
 
     const resultado = await fetch("Comprado!");
 
-    alert({
-      title: "Compra concluída",
-    });
+    // alert({
+    //   title: "Compra concluída",
+    // });
+    
+    Swal.fire({
+      title:"Cinema João🍿",
+      text:"Ingresso Comprado!",
+      icon: 'info',
+      iconColor: 'lime',
+      confirmButtonText:"Ok",
+      confirmButtonColor: 'lime',
+    })
   };
 
   return (
